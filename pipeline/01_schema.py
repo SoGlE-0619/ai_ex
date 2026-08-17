f45134f67c175224903345f478f2c36921427cb2
tubi55 <tubi55@nate.com>
2026-08-17 18:32:27 +0900

    모든 테이블 정보와 컬럼 정보, 컬럼별 자료형, PK정보 확인

--- pipeline/01_schema.py
+++ pipeline/01_schema.py
@@ -66,45 +66,38 @@ def infer_pk(columns, rows):
 
 
 # FK 후보 컬럼명의 주인으로 예상되는 테이블을 찾는 함수
-# 특정 컬럼에 매칭되는 참조 대상 테이블이 있는지 찾아서 해당 컬럼이 실제 FK인지 판단하기 위해 필요
 def owner_of(column, tables):
-  # column : "customer_id", 첫번째 인자엔 FK인지 검사하고 싶은 컬럼명이 들어옴
-  # tables : {"customsers":{...}, "products: {...}"}, 두번째 인자엔 모든 csv파일 정보값이 들어옴
-  # 일단 컬럼명에서 뒤에 _id를 제거함 (중요 :3을 :-3으로 변경)
   stem = column[:-3]
-
-  # 컬럼명이 customer 일때, 관련 테이블명이 보통 customer이거나 그 뒤에 복수형표시가 붙는게 일반적이므로 s, es를 붙인 후보군을 2개 더 생성해서 반복
-  for candidate in (stem, stem+"s",  stem+"es"):
-    # 해당 후보군에서 실제 매칭되는 테이블명이 tables정보에 있으면 해당 값을 반환
+  for candidate in (stem, stem+"s",  stem+"es"):  
     if candidate in tables:
       return candidate
-
-  # 없으면 None 반환  
   return None
 
 
-# 실제 모든 csv파일 정보를 가져와서 특정 컬럼명의 키가 주인인 테이블 명 찾는 구문
-# 모든 csv파일을 반복돌며 rows데이터를 가져와서 다시 하나의 딕셔너리로 묶어주는 구문
-# 먼저 모든 테이블 정보를 {파일명:{...}, 파일명: {...}, 파일명: {...}, 파일명: {...} } 형태로 담을 빈 딕셔너리 생성
+# 1. 모든 테이블별 필드, 데이터타입, PK구하는 실행
+# 이제부터 모든 csv파일을 반복 돌면서 각 csv파일로부터 컬럼명, 각 데이터를 read_csv함수로 추출
+# 이후 추출한 데이터를 다시 tables라는 변수에 중첩 딕셔너리 형태로 저장
+# 각 딕셔너리에는 csv파일별로 (컬럼명, 모든 데이터 행, 각 컬럼별 타입명, 해당 테이블의 PK 정보)를 담을 예정
+
+# 먼저 데이터가 담길 빈 딕셔너리 생성
 tables = {}
 
-# data폴더 안쪽의  모든 csv파일을 glob로 읽어서 반복하며 각 csv파일의 path경로를 구함
-for path in DATA_DIR.glob("*.csv"):
-  # 반복되는각 csv파일의 path경로를 read_csv함수에 전달해서 각 파일당, columns, rows 정보를 가져옴
+# data폴더 안쪽의 모든 csv파일을 glob로 찾아서 반복돌며 파일 순서대로 정렬해서 각 파일 경로를 전달
+for path in sorted(DATA_DIR.glob("*.csv")):
+  # 전달받은 path값을 인자로 전달해 read_csv함수를 호출해 컬럼명, 데이터 정보를 변수에 담음
   columns, rows = read_csv(path)
 
-  # 미리 만든 빈 딕셔너리에 키값으로 path.stem으로 csv파일에서 확장자를 제외한 파일명으로 키를 등록
-  # 그리고 각 키별로 columns에는 키값, rows에는 모든 행의 데이터를 저장
   tables[path.stem] = {
-    "columns": columns,
-    "rows":rows
+    "columns": columns, # 각 csv파일의 모든 컬럼명을 리스트로 담음
+    "rows": rows, # 모든 데이터행을 딕셔너리가 포함된 리스트 형태로 담음
+    "type": {col: infer_type([r[col] for r in rows]) for col in columns}, # 모든 컬럼값을 infer_type함수로 추론해 지정될 타입명 정보 담음
+    "pk": infer_pk(columns, rows) # infer_pk함수를 이용해 해당 테이블 정보에서의 PK값 구해서 담음
   }
 
-# 완성된 모든 csv파일의 중첩 딕셔너리 구조를 확인
-# print(tables)
-
-# 그럼 이제 모든 테이블 정보가 들어가 있는 tables에서 특정 컬럼명을 넣어서 해당 컬럼명에 해당하는 PK, FK후보가 주인인 참조당하는 테이블 명 확인
-owner = owner_of("customer_id", tables)
-print(owner) # customers  : 결국 customer_id가 PK인 테이블 명은 customers임
-
+# print(tables) # 합쳐진 모든 테이블 정보 확인
+# print(tables["customers"]) # 그중에서 customers 테이블 정보만 확인
+print(tables["customers"]["columns"]) # customers 테이블에서 컬럼명만 확인
+print(tables["customers"]["rows"][0]) # customers 테이블에서 첫번째 row행 데이터만 확인
+print(tables["customers"]["type"]) # customers 테이블에서 각 컬럼에 담길 데이터타입을 문자열로 확인
+print(tables["customers"]["pk"]) # customers 테이블에서의 PK값 확인
 
