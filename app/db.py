f24f32cc5427cd56b0f0e2af3daf252644be0d87
tubi55 <tubi55@nate.com>
2026-08-18 11:25:17 +0900

    각 데이터 행의 필드값만 커서객체의 description으로 추출

--- app/db.py
+++ app/db.py
@@ -21,10 +21,27 @@ def query(sql, params=()):
 def one(sql, params=()):
   return con.execute(sql, params).fetchone()
 
-# rows = query("SELECT name FROM products WHERE price >=? LIMIT 3", ("10000",))
-# print(rows)
+# 컬럼명이 붙은 딕셔너리 목록으로 꺼내주는 함수
+def dicts(sql, params=()):
+  # con.execute로 반환된 결과값에서 fetchone, fetchall로 꺼내지 않는 객체를 Cursor라고 함
+  # Cursor : description, fetchall(), fetchone()
+  # Cursor객체의 description에는 각 컬럼의 정보가 담겨있음
+  cur = con.execute(sql, params)
+  columns = [c[0] for c in cur.description]
+  print(columns)
 
-# C001이라는 아이디의 고객정보를 가져오는 구문
-info = one("SELECT * FROM customers WHERE customer_id = ?", ("C001",))
-print(info)
+
+# 해당 파일의 함수는 보통 다른 파일에서 해당 함수를 각각 import해서 다양하게 조합할때 쓰는 용도
+# 지금 해당 파일을 직접 실행해서 결과값을 테스트하기위해 직접 호출구문을 아래처럼 넣어버리면
+# 추후 다른 파일에서 해당 함수 import시 해당 구문이 같이 실행됨
+# 지금 파일을 직접 테스트용도로 호출할때에만 아래 구문이 실행되도록 제한을 걸어둬야 함
+
+# 아래 구문은 직접 python 명령어로 해당 파일을 호출할때 걸리게 되는 조건문
+# 다른 파일에서 해당 파일의 함수를 단지 import해서 호출시에는 아래 테스트문이 실행되지 않음
+if __name__ == "__main__":
+  # C001이라는 아이디의 고객정보를 가져오는 구문
+  info = one("SELECT * FROM customers WHERE customer_id = ?", ("C001",))
+  print(info)
+
+  dicts("SELECT * FROM customers LIMIT 1")
 
