fc26a2789237eb1a66db3993fe58945ee16c7848
tubi55 <tubi55@nate.com>
2026-08-24 10:41:19 +0900

    각각의 제품정보를 벡터화 처리

--- try_profile.py
+++ try_profile.py
@@ -26,3 +26,12 @@ pids = [r[0] for r in rows]
 print(pids)
 
 # 상품 아이디를 제외한 나머지 상품정보를 하나의 문자열로 묶어서 리스트로 반환
+# 반환된 각각의 리스트를 벡터화 처리
+product_vectors = np.array(model.embed_documents(
+[
+    " / ".join(str(x) for x in r[1:]) # 첫번재 제품아이디를 제외한 나머지 값들을 문자화 시킨뒤 하나의 문자열로 이어붙임
+    for r in rows # 각레코드를 하나씩 추출해서 
+]
+), dtype="float32") 
+
+print(product_vectors[0])
